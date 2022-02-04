bc08b51e5b5dcfd5e2837823cc039a949688361a
RajatMehta <rajat.mehta@codeaegis.in>
2022-02-04 16:35:26 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.scss"
 import App from "./App"
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store"
@@ -10,11 +10,11 @@ import { store, persistor } from "./redux/store"
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <HashRouter>
+            <BrowserRouter>
                 <PersistGate persistor={persistor}>
                     <App />
                 </PersistGate>
-            </HashRouter>
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>,
     document.getElementById("root")
